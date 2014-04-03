2702fc23768e56a9dc6157f00059b26a9175ebee
ylc.alan <ylc.alan@65e21955-ba57-a23b-a7bc-9b6e66277287>
2014-04-03 15:41:40 +0000

    Several methods added, should investigate whether moving them to Course class. Return type of add, drop, swap courses changed to TimetableError. Test cases of Timetable should be rewritten.

--- OfCourse/src/ofcourse/Timetable.java
+++ OfCourse/src/ofcourse/Timetable.java
@@ -1,15 +1,18 @@
 package ofcourse;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.awt.Color;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
-
+import javax.swing.JOptionPane;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
@@ -33,31 +36,57 @@ public class Timetable implements ofcoursegui.CourseSelectListener {
 		table_id = tid;
 	}
 	
+	/**
+	 * Constructor, new tab page will be created
+	 * @param tid
+	 * @param targetTabbedPage
+	 */
 	public Timetable(int tid, JTabbedPane targetTabbedPage) {
 		table_id = tid;
 		targetTabbedPage.add(Integer.toString(tid), gui);
 	}
 	
+	/**
+	 * Getter of table ID
+	 * @return <tt>this.table_id</tt>
+	 */
 	public int getTableId() {
 		return table_id;
 	}
 	
+	/** 
+	 * Setter of table ID
+	 * @param id New table ID (integer)
+	 */
 	public void setTableId(int id) {
 		table_id = id;
 	}
-		
+	
 	public ofcoursegui.TimeTableGUI getGUI() {
 		return gui;
 	}
+
 	
+	/**
+	 * Getter of <tt>this.enrolled</tt>
+	 * @return <tt>this.enrolled</tt>
+	 */
 	public HashMap<Course, ArrayList<Course.Session>> getEnrolled() {
 		return enrolled;
 	}
 
+	/**
+	 * Set <tt>this.enrolled</tt> to <tt>enrolled</tt>
+	 * @param enrolled
+	 */
 	public void setEnrolled(HashMap<Course, ArrayList<Course.Session>> enrolled) {
 		this.enrolled = enrolled;
 	}
 
+	/** 
+	 * Get all occupied time slots of the time table
+	 * @return ArrayList of TimeSlots occupied by courses in the time table
+	 */
 	public ArrayList<TimeSlot> getOccupied() { 
 		ArrayList<TimeSlot> arrList = new ArrayList<TimeSlot>();
 		for (java.util.Map.Entry<Course, ArrayList<Course.Session>> enrolledCourse : getEnrolled().entrySet()) {
@@ -71,6 +100,11 @@ public class Timetable implements ofcoursegui.CourseSelectListener {
 		return arrList;
 	}
 	
+	/** 
+	 * Convert a list of sessions to a list of time slots occupied by <tt>sessions</tt>
+	 * @param sessions Array of course sessions
+	 * @return Array List of <tt>TimeSlot</tt> of those occupied by <tt>sessions</tt>
+	 */
 	private ArrayList<TimeSlot> sessionsToSlots(Course.Session[] sessions) {
 		//List all time slots to be used by this course registration
 		ArrayList<TimeSlot> timeSlots = new ArrayList<TimeSlot>();
@@ -84,10 +118,112 @@ public class Timetable implements ofcoursegui.CourseSelectListener {
 		return timeSlots;
 	}
 	
-	public boolean addCourse(String course_id, String[] sessions_id) {
+	/**
+	 * Check whether the array list of sessions contain all session types and no duplicate session types
+	 * @param course
+	 * @param sessions
+	 * @return 0 if all sessions are included and distinct, 1 if session type missing, 2 if duplicate session type
+	 */
+	public int containsAllSessionTypes(Course course, ArrayList<Session> sessions) {
+		ArrayList<SessionType> stype = new ArrayList<SessionType>(), ctype = new ArrayList<SessionType>();
+		for (Course.Session s : course.getSessions()) {
+			if (!stype.contains(s.getSType())) {
+				stype.add(s.getSType());
+			}
+		}
+		for (Course.Session s : sessions) {
+			if (!ctype.contains(s.getSType())) {
+				ctype.add(s.getSType());
+			}
+			else { // multiple same session type
+				return 2;
+			}
+		}
+		if (!ctype.containsAll(stype)) {
+			return 1;
+		}
+		return 0;
+	}
+	
+	/**
+	 * Check if <tt>course</tt> is enrolled in this time table
+	 * @param course
+	 * @return TRUE if <tt>course</tt> enrolled, FALSE if <tt>course</tt> not enrolled
+	 */
+	public boolean courseEnrolled(Course course) {
+		return getEnrolled().containsKey(course);
+	}
+	
+	/**
+	 * Get time slots that are conflicted with course enrolled
+	 * @param sessions Sessions to be enrolled
+	 * @return ArrayList of <tt>TimeSlot</tt> that conflicts with course enrolled, NULL if no conflicts
+	 */
+	public ArrayList<TimeSlot> getConflictedSlots(ArrayList<Course.Session> sessions) {
+		//List all time slots to be used by this course registration
+		ArrayList<TimeSlot> timeSlots = sessionsToSlots(sessions.toArray(new Course.Session[sessions.size()]));
+		ArrayList<TimeSlot> occupied = getOccupied();
+		ArrayList<TimeSlot> conflictSlots = new ArrayList<TimeSlot>();
+		boolean conflict = false;
+		for (int i = 0; i < timeSlots.size() && !conflict; i++) {
+			if (occupied.contains(timeSlots.get(i))) {
+				conflict = true;
+				conflictSlots.add(timeSlots.get(i));
+			}
+		}
+		if (!conflict) return null;
+		return conflictSlots;
+	}
+	
+	/** 
+	 * Check whether time conflicts exist between each session in the array list <tt>sessions</tt> 
+	 * @param sessions
+	 * @return TRUE if no conflicts, FALSE if conflicts exist
+	 */
+	public boolean noSelfConflicts(ArrayList<Course.Session> sessions) {
+		//List all time slots to be used by this course registration
+		ArrayList<TimeSlot> timeSlots = sessionsToSlots(sessions.toArray(new Course.Session[sessions.size()]));
+		// check time conflict within itself
+		// efficiency is not a matter: the array tends to be small
+		for (int i = 0; i < timeSlots.size(); i++) {
+            for (int j = 0; j < timeSlots.size(); j++) {
+                if (timeSlots.get(i).equals(timeSlots.get(j)) && i != j) {
+                    return false;
+                }
+            }
+        }
+		return true;
+	}
+	
+	
+	/**
+	 * Check whether lecture are matched with tutorial or lab, TRUE when matching is not required
+	 * @param course
+	 * @param sessions
+	 * @return TRUE when matching is not required or sessions matched, FALSE when sessions unmatched
+	 */
+	public boolean sessionsMatched(Course course, ArrayList<Course.Session> sessions) {
+		if (!course.isMatchSession()) { // no need to match session type
+			return true;
+		}
+		int sNo = sessions.get(0).getSNo();
+		for (Course.Session s : sessions) {
+			if (sNo != s.getSNo())
+				return false;
+		}
+		return true;
+	}
+	
+	/** 
+	 * Add course with <tt>course_id</tt> and specified sessions (<tt>sessions_id</tt>)
+	 * @param course_id Course ID of the course
+	 * @param sessions Array of String of session ID
+	 * @return 
+	 */
+	public TimetableError addCourse(String course_id, String[] sessions_id) {
 		ArrayList<Course.Session> sessions = new ArrayList<Course.Session>();
 		Course c = Course.getCourseByName(course_id);
-		if (c == null) return false;
+		if (c == null) return TimetableError.CourseNotExists;
 		for (Course.Session s : c.getSessions()) {
 			for(String sessionid : sessions_id) {
 				//Debug System.out.println(sessionid + " " + s.toString());
@@ -98,76 +234,53 @@ public class Timetable implements ofcoursegui.CourseSelectListener {
 			}
 		}
 		if (sessions == null || sessions.size() == 0) 
-			return false;
+			return TimetableError.InvalidSessions;
 
 		return addCourse(c, sessions.toArray(new Course.Session[sessions.size()]));
 	}
-	
-	public boolean addCourse(Course course, Course.Session[] sessions) {
+
+	/** 
+	 * Add <tt>Course</tt> with specified <tt>sessions</tt>
+	 * @param course
+	 * @param sessions
+	 * @return 
+	 */
+	public TimetableError addCourse(Course course, Course.Session[] sessions) {
 		// if the course has already been enrolled, fails
-		if (getEnrolled().containsKey(course)) {
-			//Debug System.out.println("0reached");
-			return false;
+		if (courseEnrolled(course)) {
+			return TimetableError.CourseEnrolled;
 		}
 		// ensure all provided sessions belong to the course
 		ArrayList<Course.Session> trueSessions = new ArrayList<Course.Session>();
 		for (Course.Session s : sessions) {
 			if(course.getSessions().contains(s)) trueSessions.add(s);
 		}
-		
-		// advanced checking for if all session types (lecture/lab/tutorial) are included
-		ArrayList<SessionType> stype = new ArrayList<SessionType>(), ctype = new ArrayList<SessionType>();
-		for (Course.Session s : course.getSessions()) {
-			if (!stype.contains(s.getSType())) {
-				stype.add(s.getSType());
-			}
+				int flag = containsAllSessionTypes(course, trueSessions);
+		if (flag==1) { // check if all session types (lecture/lab/tutorial) are included
+			return TimetableError.SessionTypeMissed;
 		}
-		for (Course.Session s : trueSessions) {
-			if (!ctype.contains(s.getSType())) {
-				ctype.add(s.getSType());
-			}
+		else if (flag==2) {
+			return TimetableError.DuplicateSessionType;
 		}
-		if (!ctype.containsAll(stype)) {
-			//Debug System.out.println("1reached");
-			return false;
-		}
-		
-		// TODO: check whether matching between lecture, tutorial, and lab is needed
-		
-		//List all time slots to be used by this course registration
-		ArrayList<TimeSlot> timeSlots = sessionsToSlots(trueSessions.toArray(new Course.Session[trueSessions.size()]));
-		
-		// check time conflict within itself
-		// efficiency is not a matter: the array tends to be small
-		for (int i = 0; i < timeSlots.size(); i++) {
-            for (int j = 0; j < timeSlots.size(); j++) {
-                if (timeSlots.get(i).equals(timeSlots.get(j)) && i != j) {
-                	//Debug System.out.println("2reached");
-                    return false;
-                }
-            }
-        }
-		// check time conflict with enrolled
-		ArrayList<TimeSlot> occupied = getOccupied();
-		boolean conflict = false;
-		for (int i = 0; i < timeSlots.size() && !conflict; i++) {
-			if (occupied.contains(timeSlots.get(i))) {
-				conflict = true;
-			}
-		}
-		if (conflict) {
-			//Debug System.out.println("3reached");
-			return false;
-		}
-		else {
-			//put it to enrolled
-			ArrayList<Course.Session> ss = new ArrayList<Course.Session>();
-			ss.addAll(trueSessions);
-			//duplicate courses are automatically overwritten
-			enrolled.put(course, ss);
-		}
+		if (!sessionsMatched(course, trueSessions)) { // check matching between lecture, tutorial, and lab is needed
+			return TimetableError.SessionsNotMatched;
 
-		//GUI call
+		}
+		if (!noSelfConflicts(trueSessions)) {
+			return TimetableError.SelfConflicts;
+		}
+		if (getConflictedSlots(trueSessions)!=null) {
+			return TimetableError.TimeConflicts;
+		}
+		
+		
+		// put it to enrolled
+		ArrayList<Course.Session> ss = new ArrayList<Course.Session>();
+		ss.addAll(trueSessions);
+		// duplicate courses are automatically overwritten
+		enrolled.put(course, ss);
+
+		// GUI call
 		Color c = TimeTableGUI.getRandomBgColor();
 		for (Course.Session s : trueSessions) {
 			for (TimePeriod tp : s.getSchedule()) {
@@ -184,33 +297,50 @@ public class Timetable implements ofcoursegui.CourseSelectListener {
 			}
 		}
 		
-		return true;
+		return TimetableError.NoError;
 	}
 	
-	public boolean dropCourse(String course_id) {
+	/**
+	 * Drop <tt>course</tt> from the timetable, and trigger GUI repaint
+	 * @param course_id Course ID of course to be dropped
+	 * @return 
+	 */
+	public TimetableError dropCourse(String course_id) {
 		return dropCourse(Course.getCourseByName(course_id));
 	}
 	
-	public boolean dropCourse(Course course) {
+	/**
+	 * Drop <tt>course</tt> from the timetable, and trigger GUI repaint
+	 * @param course Course to be dropped
+	 * @return 
+	 */
+	public TimetableError dropCourse(Course course) {
 		if (course == null) 
-			return false;
-		if (!getEnrolled().containsKey(course)) {
-			return false;
+			return TimetableError.CourseNotExists;
+		if (!courseEnrolled(course)) {
+			return TimetableError.CourseNotEnrolled;
 		}
 		else {
 			getEnrolled().remove(course);
 		}
 		//GUI call
 		gui.unfillSlots(course.toString());
-		return true;
+		return TimetableError.NoError;
 	}
 	
-	public boolean swapCourse(Course origin, Course target, Course.Session[] sessions) {
+	/**
+	 * Swap course <tt>origin</tt> with <tt>target</tt>
+	 * @param origin Original course
+	 * @param target Target course to replace original course
+	 * @param sessions Sessions of target course to be enrolled
+	 * @return TRUE if swap success, FALSE if swap fail
+	 */
+	public TimetableError swapCourse(Course origin, Course target, Course.Session[] sessions) {
 		boolean conflict = false;
 		
 		// if target course has already been enrolled, fails
-		if (getEnrolled().containsKey(target)) {
-			return false;
+		if (courseEnrolled(target)) {
+			return TimetableError.CourseEnrolled;
 		}
 		//List all time slots to if the original course is dropped
 		ArrayList<TimeSlot> remainingSlots = new ArrayList<TimeSlot>();
@@ -234,24 +364,25 @@ public class Timetable implements ofcoursegui.CourseSelectListener {
 		}
 		// conflict even after dropping
 		if (conflict) {
-			return false;
+			return TimetableError.TimeConflicts;
 		}
 		else {
 			ArrayList<Course.Session> temp = getEnrolled().get(origin);
-			if(dropCourse(origin) == false) return false;
-			if(addCourse(target, sessions) == false) {
+			TimetableError er = dropCourse(origin);
+			if(er != TimetableError.NoError) return er;
+			if(addCourse(target, sessions) != TimetableError.NoError) {
 				addCourse(origin, temp.toArray(new Course.Session[temp.size()]));
 			}
 		}
 
-		return true;
+		return TimetableError.NoError;
 	}
 	
-	/** Export the attributes of the timetable (TID, Enrolled). Enrolled courses are exported as a list of class numbers.
-	 * 
+	/** 
+	 * Export the attributes of the timetable (TID, Enrolled). Enrolled courses are exported as a list of class numbers.
 	 * @return A String which can be split for import
 	 */
-	public String exportTable() {
+	public String exportString() {
 		String returnStr = new String(this.getTableId()+delim);
 		HashMap<Course, ArrayList<Course.Session>> enrolled = this.getEnrolled();
 		Iterator<ArrayList<Session>> it = enrolled.values().iterator();
@@ -265,12 +396,12 @@ public class Timetable implements ofcoursegui.CourseSelectListener {
 		return returnStr;
 	}
 	
-	/** Import timetable from String, replacing the original one, unchanged if failed
-	 * 
+	/** 
+	 * Import timetable from String, replacing the original one, unchanged if failed
 	 * @param instr (should consist of integers only)
-	 * @return True if the timetable is replaced by the import one, False means import failure and the timetable is unchanged
+	 * @return TRUE if the timetable is replaced by the import one, FALSE means import failure and the timetable is unchanged
 	 */
-	public boolean importTable(String instr) {
+	public boolean importString(String instr) {
 		String[] tuples = instr.trim().split(delim);
 		try {
 			Integer.parseInt(tuples[0]);
@@ -308,7 +439,7 @@ public class Timetable implements ofcoursegui.CourseSelectListener {
 				}
 			}
 			if (consistent) {
-				addSuccess = this.addCourse(acourse, sessions);
+				addSuccess = (this.addCourse(acourse, sessions)==TimetableError.NoError);
 			}
 		}
 		if (!consistent || !addSuccess) {
@@ -319,54 +450,76 @@ public class Timetable implements ofcoursegui.CourseSelectListener {
 		return true;
 	}
 	
-//	public String exportString() {
-//		String returnStr = new String();
-//		HashMap<Course, ArrayList<Course.Session>> map = getEnrolled();
-//		Comparator<Course> comparator = new Comparator<Course>() {
-//			public int compare(Course c1, Course c2) {
-//				return (c1.toString().compareTo(c2.toString()));
-//			}
-//		};
-//		SortedSet<Course> keys = new TreeSet<Course>(comparator);
-//		keys.addAll(map.keySet());
-//		int count = 0, size = map.size();
-//		for (Course key : keys) {
-//			count++;
-//			ArrayList<Course.Session> value = map.get(key);
-//			returnStr += key.toString() + delim;
-//			int count2 = 0, size2 = value.size();
-//			for (Course.Session s : value) {
-//				count2++;
-//				returnStr += s.toString();
-//				if (count2 != size2) returnStr += innerDelim;
-//			}
-//			if (count != size) returnStr += delim;
-//		}
-//		return returnStr;
-//	}
-//	
-//	public boolean importFrom(String inStr) {
-//		String tmp[] = inStr.split(delim);
-//		String tmpSs[] = null;
-//		if (tmp.length % 2 != 0) return false;
-//		for (int i=0; i<tmp.length; i+=2) {
-//			tmpSs = tmp[i+1].split(innerDelim);
-//			addCourse(tmp[i], tmpSs);
-//		}
-//		return true;
-//	}
+	/** 
+	 * Write the string from method <tt>exportString()</tt> to file named <tt>filename</tt>
+	 * @param filename
+	 * @return TRUE if write success, FALSE if any IO exceptions occur 
+	 */
+	public boolean exportFile(String filename) {
+		BufferedWriter bw = null;
+		try {
+			bw = new BufferedWriter(new FileWriter(filename));
+			String line = exportString();
+			bw.write(line + "\n");
+		}
+		catch (FileNotFoundException ex) {
+			//System.out.println(">> Error: File ("+ filename +") Not Found");
+			return false;
+		}
+		catch (IOException ex) {
+			//System.out.println(">> Error: File ("+ filename +") Writing Error");
+			return false;
+		}
+		finally {
+			try {
+				if (bw!=null) bw.close();
+			} catch (IOException e) {
+				System.out.println("Fatal error: fail to close file (" + filename + ")");
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	/** 
+	 * Import timetable from String obtained from file named <tt>filename</tt>, replacing the original one, unchanged if failed. 
+	 * Make use of the method <tt>importString(String line)</tt>
+	 * @param filename
+	 * @return TRUE if the timetable is replaced by the import one, FALSE means import failure and the timetable is unchanged
+	 */
+	public boolean importFile(String filename) {
+		ArrayList<String> strArr = new ArrayList<String>();
+		BufferedReader br = null;
+		boolean importStatus = false;
+		try {
+			br = new BufferedReader(new FileReader(filename));
+			String line = br.readLine();
+			while (line != null) {
+				strArr.add(line.trim());
+				line = br.readLine();
+			}
+			importStatus = importString(strArr.get(0));
+		}
+		catch (FileNotFoundException ex) {
+			//System.out.println(">> Error: File ("+ filename +") Not Found");
+			return false;
+		}
+		catch (IOException ex) {
+			//System.out.println(">> Error: File ("+ filename +") Reading Error");
+			return false;
+		}
+		finally {
+			try {
+				if (br!=null) br.close();
+			} catch (IOException e) {
+				System.out.println("Fatal error: fail to close file (" + filename + ")");
+				return false;
+			}
+		}
+		return importStatus;
+	}
 	
 	public static String delim = ";", innerDelim = ",";
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 	private Course selectedCourse = null;
 	
@@ -394,5 +547,4 @@ public class Timetable implements ofcoursegui.CourseSelectListener {
 		if (selectedCourse != null) this.gui.unselectSlots(selectedCourse.getCode().toString());
 		selectedCourse = null;
 	}
-
 }
